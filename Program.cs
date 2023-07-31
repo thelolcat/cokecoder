686f58f84fb2bc051e66df0446765da141938e9e
lolcat <105110312+thelolcat@users.noreply.github.com>
2023-07-31 17:16:27 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,125 +6,148 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("CokeCoder v1.1 | C# Symmetric Encoder");
+            Console.WriteLine("CokeCoder v2.0");
             
-            // declares variables (before loop)
-            string function;
-            string encrypt;
-            int codeKey;
-            int coder;
-            int i = 1;
-            double result;
-            char code;
-            int digit1;
-            int digit2;
-            int digit3;
-            
-            // determines function
-            Console.WriteLine("Encode/Decode/Generate Key/Help");
-            function = Console.ReadLine();
-            function = function.ToLower();
-            
-            // generates key
-            if (function.Contains("generate"))
-            {
-                Console.WriteLine("Enter a random number between 1000 and 9999");
-                if (double.TryParse(Console.ReadLine(), out result) && result > 1000 && result < 9999)
-                {
-                    result = Math.Pow(result, 3); // input raised to the power of 3
-                    Console.WriteLine($"New key -> {result % 100}"); // prints key (remainder on dividing result by 100)
-                    Console.ReadKey();
-                }
-                else // responds to an invalid input
-                {
-                    Console.WriteLine("Invalid input: expected double between 1000 and 9999");
-                    Console.ReadKey();
-                }
-            }
-            else if (function.Contains("encode"))
-            {
-                Console.WriteLine("Enter key"); // input codekey
-                codeKey = int.Parse(Console.ReadLine());
-                
-                Console.WriteLine("Enter message"); // input message to be encrypted
-                encrypt = Console.ReadLine();
+            // declaring variables
+            string command;
+            string text;
+            char textBreakUp;
+            int counter = 1;
+            int key = 1;
+            int textConvert;
+            int result;
+            int i;
 
-                for (i = 0; i < encrypt.Length; i++)
+            while (counter == 1) // carries on looping until you do not input
+            {
+                Console.WriteLine("Enter a command or type '/help' for a list of commands");
+                command = Console.ReadLine() + " /end";
+
+                // the help command lists out every command useful
+                if (command.StartsWith("/help"))
                 {
-                    // encrypts each char by adding codekey*(i-1) to the int of the char
-                    coder = (int)encrypt[i];
-                    coder = coder + (codeKey * (i + 1));
-                    
-                    while (coder > 999) // minus 1000 if coder is above 999
-                    {
-                        coder = coder - 1000;
-                    }
-                    
-                    while (coder < 100) // prints a zero before the int if int is below 100
-                    {
-                        Console.Write("0");
-                    }
-                    Console.Write(coder);
+                    Console.WriteLine("/encode <string> -> Encodes text with a key");
+                    Console.WriteLine("/decode <string> -> Decodes text encoded with given key");
+                    Console.WriteLine("/key [int] -> Sets the encoding key to given number");
+                    Console.WriteLine("/guide -> Prints information on the program and troubleshooting tips");
+                }
+
+                // the guide command gives info on particulars of the program
+                else if (command.StartsWith("/guide"))
+                {
+                    Console.WriteLine("Program info:");
+                    Console.WriteLine("   CokeCoder version 2.0 | Symmetric encoder");
+                    Console.WriteLine("   Console application, written in C# (153 lines)");
+                    Console.WriteLine("   Script written by _thelolcat with assistance and support from aless_dev06 and" +
+                        " furetto126");
+                    Console.WriteLine("   Last updated 27/07/2023");
+                    Console.WriteLine("  ");
+                    Console.WriteLine("If the program is not decoding properly, check if the key is accurate using /key" +
+                        " current");
+                    Console.WriteLine("If you see '?' in your encoded message, chances are that the console does not " +
+                        "support the coversion of that particular character. Try setting your key to a lower one for " +
+                        "better results");
+                    Console.WriteLine("Make sure there are no inaccuracies while entering commands");
+                    Console.WriteLine("Refer the internet for unknown program terms");
+                    Console.WriteLine("Press Enter to exit the program. Alternatively type '/end'");
                 }
                 
-                // prints encoded message
-                Console.Write($" encrypted using codekey {codeKey}.");
-                Console.WriteLine(" Restart for other commands");
-                Console.ReadKey();
-            }
-            else if (function.Contains("decode"))
-            {
-                Console.WriteLine("Enter key"); // input codekey
-                codeKey = int.Parse(Console.ReadLine());
-                
-                Console.WriteLine("Enter code"); // input secret code
-                encrypt = Console.ReadLine();
-                while (i < encrypt.Length - 2)
+                // the key command sets a key
+                else if (command.StartsWith("/key"))
                 {
-                    // assigns three digits to each int variable
-                    code = encrypt[i];
-                    digit1 = code * 100;
-                    code = encrypt[i + 1];
-                    digit2 = code * 10;
-                    code = encrypt[i + 2];
-                    digit3 = code;
+                    command = command.Replace("/key", " ");
+                    command = command.Replace("/end", " ");
+                    command = command.Trim();
+                    /* The above code will be seen in the upcoming two lines too. This line deletes the command and the
+                     * ending string from the user input so that only the program can utilise the unique input */
                     
-                    // finds out and decrypts the set of three digits
-                    coder = digit1 + digit2 + digit3;
-                    coder = coder - (codeKey * ((i + 1) / 3));
-                    while (coder < 0)
+                    if (int.TryParse(command, out result)) // checks if input is an integer
                     {
-                        coder = coder + 25;
-                    }
-
-                    // prints end result
-                    Console.Write(Convert.ToChar(coder));
-                    if (i > encrypt.Length - 2)
-                    {
-                        break;
+                        if (result > 0 && result < 8) // checks if input < 8 and > 1
+                        {
+                            key = result;
+                            Console.WriteLine($"Set key to {key}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, expected an integer between 1 and 7. Are there any " +
+                                "indeliberate mistakes in you input?");
+                        }
                     }
                     else
                     {
-                        i = i + 3;
+                        if (command.Contains("current")) // shows current key
+                        {
+                            Console.WriteLine($"The current key being used is {key}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, expected an integer between 1 and 7. Are there any indeliberate " +
+                                "mistakes in your input?");
+                        }
                     }
                 }
-                Console.WriteLine("Restart for other commands");
-                Console.ReadKey();
-            }
-            else if (function.Contains("help"))
-            {
-                Console.WriteLine("Enter 'encode' to encode a message into a secret code with a key");
-                Console.WriteLine("Enter 'decode' to decode a secret code encrypted using this encoder");
-                Console.WriteLine("Enter 'generate' to generate a key for encryption");
-                Console.WriteLine("C# (90 lines), script written by _thelolcat@Discord using Visual Studio 2019");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("Wrong command, restart and try again or enter 'help' for info");
-                Console.ReadKey();
+                
+                // the encode command encodes text with given key
+                else if (command.StartsWith("/encode"))
+                {
+                    command = command.Replace("/encode", " ");
+                    command = command.Replace("/end", " ");
+                    command = command.Trim();
+                    text = command;
+                    for (i = 0; i < text.Length; i++)
+                    {
+                        textBreakUp = text[i];
+                        textConvert = (int)textBreakUp;
+                        textConvert = textConvert + (key * ((i + 1) % 2 +1));
+                        Console.Write((char)textConvert);
+                    }
+                    if (i == 0)
+                    {
+                        Console.WriteLine("Error, expected string input in command -> /encode <string>");
+                    }
+                    else
+                    {
+                        Console.WriteLine($" encoded using key {key}");
+                    }
+                }
+                
+                // the decode command decodes text with given key
+                else if (command.StartsWith("/decode"))
+                {
+                    command = command.Replace("/decode", " ");
+                    command = command.Replace("/end", " ");
+                    command = command.Trim();
+                    text = command;
+                    for (i = 0; i < text.Length; i++)
+                    {
+                        textBreakUp = text[i];
+                        textConvert = (int)textBreakUp;
+                        textConvert = textConvert - (key * ((i + 1) % 2 + 1));
+                        Console.Write((char)textConvert);
+                    }
+                    if (i == 0)
+                    {
+                        Console.WriteLine("Error, expected string input in command -> /decode <string>");
+                    }
+                    else
+                    {
+                        Console.WriteLine($" decoded from {text} using key {key}");
+                    }
+                }
+                else
+                {
+                    command = command.Trim();
+                    if (command.StartsWith("/end")) // terminates the program
+                    {
+                        counter = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error, unknown command. Are there any indeliberate mistakes in your input?");
+                    }
+                }
             }
         }
     }
 }
-
