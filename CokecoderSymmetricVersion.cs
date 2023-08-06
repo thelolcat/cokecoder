cd33b6c5803719a40caf01c6db17cd7605ad10a4
lolcat <105110312+thelolcat@users.noreply.github.com>
2023-08-06 08:04:39 +0530

    Updated CokecoderSymmetricVersion.cs
    Minor and insignificant alterations

--- CokecoderSymmetricVersion.cs
+++ CokecoderSymmetricVersion.cs
@@ -150,11 +150,11 @@ namespace helloWorld
         public static void Guide()
         {
             Console.WriteLine("Program info:");
-            Console.WriteLine("   CokeCoder version 2.0 | Symmetric encoder");
-            Console.WriteLine("   Console application, written in C# (153 lines)");
+            Console.WriteLine("   CokeCoder version 2.2 | Symmetric encoder");
+            Console.WriteLine("   Console application, written in C# (169 lines)");
             Console.WriteLine("   Script written by _thelolcat with assistance and support from aless_dev06 and" +
                 " furetto126");
-            Console.WriteLine("   Last updated 27/07/2023");
+            Console.WriteLine("   Last updated 06/08/2023");
             Console.WriteLine("  ");
             Console.WriteLine("If the program is not decoding properly, check if the key is accurate using /key" +
                 " current");
