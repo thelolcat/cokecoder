13cc70962db718f8c48c1d1ee18b66f4f5a8031b
lolcat <105110312+thelolcat@users.noreply.github.com>
2024-02-27 08:20:24 +0530

    Update CokecoderSymmetricVersion.cs

--- CokecoderSymmetricVersion.cs
+++ CokecoderSymmetricVersion.cs
@@ -1,4 +1,5 @@
-/*MIT License
+/**************************************************************
+MIT License
 
 Copyright (c) 2024 thelolcat
 
@@ -19,7 +20,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-*/
+***************************************************************/
 
 using System;
 
